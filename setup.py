c730f2e2aba2cb7a714942c36779ff597b02157c
Kacper Murczkiewicz <71273151+kmurczkiewicz@users.noreply.github.com>
2021-12-30 21:35:10 +0100

    Remove OV functionality

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 setup(
    name='cnn_dev_app',
@@ -8,15 +8,5 @@ setup(
    author='Kacper Murczkiewicz',
    author_email='murczkiewiczkacper@gmail.com',
    url="https://github.com/kmurczkiewicz/Sentinel-2-Image-classification-using-CNNs",
-   packages=[
-       'src.data',
-       'src.executor',
-       'src.helpers',
-       'src.nn_operations',
-    ]
-   #install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
-   #scripts=[
-   #         'scripts/cool',
-    #        'scripts/skype',
-     #      ]
+   packages=find_packages(include=['src'])
 )
